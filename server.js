ae154afe7832f90526e0f2cd3a2b182d69589845
vantu28011998 <nguyenvantu28011998it@gmail.com>
2021-08-01 23:57:57 +0700

    test

--- server.js
+++ server.js
@@ -11,9 +11,26 @@ app.use(
   })
 );
 
-// Serve all the files in the '/dist' directory
+// Serve all the files in '/dist' directory
 app.use(express.static("dist"));
-var port = process.env.PORT || 8080
+
+// Force HTTPS redirection
+app.use(function(req, res, next) {
+  if (
+    req.secure ||
+    req.headers["x-forwarded-proto"] === "https" ||
+    process.env.NODE_ENV !== "production"
+  ) {
+    return next();
+  } else {
+    return res.redirect("https://" + req.headers.host + req.url);
+  }
+});
+app.get('/', function (req, res) {
+  res.render('index', {});
+});
+var port = process.env.PORT || 8081
 app.listen(port, "0.0.0.0", function() {
   console.log("My super vue app is listening on port 8080");
 });
+
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/vantu28011998/o2o-first-roast-coffee-FEapp.git/' // Thay tên repository của các bạn vào đây nhé
+      : '/'
+  }
